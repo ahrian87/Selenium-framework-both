54ce2405135723a72d703df44b32b561e80f7f4f
Maciej Zbydniewski <maciej.zbydniewski@soflab.pl>
2023-07-13 12:13:07 +0200

    added some @FindBy's in the class

--- src/test/java/BasicExamplesPage.java
+++ src/test/java/BasicExamplesPage.java
@@ -1,8 +1,34 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class BasicExamplesPage {
+
+    @FindBy(css="a[href='./basic-first-form-demo.html'][class='list-group-item']")
+    private WebElement simpleFormDemoButton;
+
+    @FindBy(css="a[href='./basic-checkbox-demo.html'][class='list-group-item']")
+    private WebElement checkBoxDemoButton;
+
+    @FindBy(css="a[href='./basic-radiobutton-demo.html'][class='list-group-item']")
+    private WebElement radioButtonsDemoButton;
+
+    @FindBy(css="a[href='./basic-select-dropdown-demo.html'][class='list-group-item']")
+    private WebElement selectDropdownListButton;
+
+    @FindBy(css="a[href='./javascript-alert-box-demo.html'][class='list-group-item']")
+    private WebElement javascriptAlertsButton;
+
+    @FindBy(css="a[href='./window-popup-modal-demo.html'][class='list-group-item']")
+    private WebElement windowPopupModalButton;
+
+    @FindBy(css="a[href='./boostrap-alert-messages-demo.html'][class='list-group-item']")
+    private WebElement bootstrapAlertsButton;
+
+    @FindBy(css="a[href='./bootstrap-modal-demo.html'][class='list-group-item']")
+    private WebElement bootstrapModalsButton;
+
     private WebDriver driver;
 
     public BasicExamplesPage(WebDriver driver) {
@@ -10,42 +36,34 @@ public class BasicExamplesPage {
     }
 
     public void clickSimpleFormDemoButton() {
-        WebElement simpleFormDemoButton = driver.findElement(By.cssSelector("a[href='./basic-first-form-demo.html'][class='list-group-item']"));
         simpleFormDemoButton.click();
     }
 
     public void clickCheckBoxDemoButton() {
-        WebElement checkBoxDemoButton = driver.findElement(By.cssSelector("a[href='./basic-checkbox-demo.html'][class='list-group-item']"));
         checkBoxDemoButton.click();
     }
 
     public void clickRadioButtonsDemoButton() {
-        WebElement radioButtonsDemoButton = driver.findElement(By.cssSelector("a[href='./basic-radiobutton-demo.html'][class='list-group-item']"));
         radioButtonsDemoButton.click();
     }
 
     public void clickSelectDropdownListButton() {
-        WebElement selectDropdownListButton = driver.findElement(By.cssSelector("a[href='./basic-select-dropdown-demo.html'][class='list-group-item']"));
         selectDropdownListButton.click();
     }
 
     public void clickJavascriptAlertsButton() {
-        WebElement javascriptAlertsButton = driver.findElement(By.cssSelector("a[href='./javascript-alert-box-demo.html'][class='list-group-item']"));
         javascriptAlertsButton.click();
     }
 
     public void clickWindowPopupModalButton() {
-        WebElement windowPopupModalButton = driver.findElement(By.cssSelector("a[href='./window-popup-modal-demo.html'][class='list-group-item']"));
         windowPopupModalButton.click();
     }
 
     public void clickBootstrapAlertsButton() {
-        WebElement bootstrapAlertsButton = driver.findElement(By.cssSelector("a[href='./boostrap-alert-messages-demo.html'][class='list-group-item']"));
         bootstrapAlertsButton.click();
     }
 
     public void clickBootstrapModalsButton() {
-        WebElement bootstrapModalsButton = driver.findElement(By.cssSelector("a[href='./bootstrap-modal-demo.html'][class='list-group-item']"));
         bootstrapModalsButton.click();
     }
 }
