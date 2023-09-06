27e0138063c952e087cf56f861f5037acf133053
ahrian87 <maciej.zbydniewski@gmail.com>
2023-09-06 19:57:02 +0200

    new page for radio buttons demo

---
+++ src/test/java/page/objects/RadioButtonsPage.java
@@ -0,0 +1,36 @@
+package page.objects;
+
+import driver.manager.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import waits.WaitForElement;
+
+public class RadioButtonsPage {
+
+    private Logger logger = LogManager.getRootLogger();
+
+    @FindBy(css="input[value='Male'][name='optradio']")
+    WebElement maleButton;
+
+    @FindBy(css="input[value='Female'][name='optradio']")
+    WebElement femaleButton;
+
+    public RadioButtonsPage() {
+        PageFactory.initElements(DriverManager.getWebDriver(), this);
+    }
+
+    public void clickMaleButton() {
+        WaitForElement.waitUntilElementIsVisible(maleButton);
+        maleButton.click();
+        logger.info("Click on Male button");
+    }
+
+    public void clickFemaleButton() {
+        WaitForElement.waitUntilElementIsVisible(femaleButton);
+        femaleButton.click();
+        logger.info("Click on Female button");
+    }
+}
