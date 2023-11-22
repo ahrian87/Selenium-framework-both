51217aff98261f8fd31a9f3714de63fb2c84a07a
Maciej Zbydniewski <maciej.zbydniewski@soflab.pl>
2023-11-22 14:25:52 +0100

    SimpleFormTests refactor - according to fluent interface

--- src/test/java/tests/SimpleFormTests.java
+++ src/test/java/tests/SimpleFormTests.java
@@ -13,32 +13,25 @@ public class SimpleFormTests extends TestBase{
         LandingPage landingPage = new LandingPage();
         landingPage
                 .clickOnStartButton()
-                .clickSimpleFormDemoButton();
-
+                .clickSimpleFormDemoButton()
+                .typeIntoInputMessageField("hello world")
+                .clickShowMessageButton();
         SimpleFormPage simpleFormPage = new SimpleFormPage();
-        simpleFormPage.typeIntoInputMessageField("hello world");
-        simpleFormPage.clickShowMessageButton();
-
         String message = simpleFormPage.getMessage();
-
         assertEquals(message, "hello world");
     }
 
     @Test
     public void fillingTwoInputFieldsFormTest() {
         LandingPage landingPage = new LandingPage();
-        landingPage.clickOnStartButton();
-
-        BasicExamplesPage basicExamplesPage = new BasicExamplesPage();
-        basicExamplesPage.clickSimpleFormDemoButton();
-
+        landingPage
+                .clickOnStartButton()
+                .clickSimpleFormDemoButton()
+                .typeIntoEnterAInputField("2")
+                .typeIntoEnterBInputField("3")
+                .clickGetTotalButton();
         SimpleFormPage simpleFormPage = new SimpleFormPage();
-        simpleFormPage.typeIntoEnterAInputField("2");
-        simpleFormPage.typeIntoEnterBInputField("3");
-
-        simpleFormPage.clickGetTotalButton();
         String totalValue = simpleFormPage.getTotalValue();
-
         assertEquals(totalValue, "5");
     }
 }
