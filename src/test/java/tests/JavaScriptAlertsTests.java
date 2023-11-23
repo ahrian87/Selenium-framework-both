fe18b1778c19fa88563e8321b8cb530476c52c86
Maciej Zbydniewski <maciej.zbydniewski@soflab.pl>
2023-11-23 08:07:47 +0100

    JavaScriptAlertsTests refactor - according to fluent interface, dead code removal

--- src/test/java/tests/JavaScriptAlertsTests.java
+++ src/test/java/tests/JavaScriptAlertsTests.java
@@ -1,7 +1,6 @@
 package tests;
 
 import org.testng.annotations.Test;
-import page.objects.BasicExamplesPage;
 import page.objects.JavaScriptAlertsPage;
 import page.objects.LandingPage;
 import waits.OtherWaits;
@@ -14,75 +13,53 @@ public class JavaScriptAlertsTests extends TestBase {
     @Test
     public void closeAlertBoxTest() {
         LandingPage landingPage = new LandingPage();
-        landingPage.clickOnStartButton();
-
-        BasicExamplesPage basicExamplesPage = new BasicExamplesPage();
-        basicExamplesPage.clickJavascriptAlertsButton();
-
+        landingPage
+                .clickOnStartButton()
+                .clickJavascriptAlertsButton()
+                .clickJavaScriptAlertBoxButton();
         JavaScriptAlertsPage javaScriptAlertsPage = new JavaScriptAlertsPage();
-        javaScriptAlertsPage.clickJavaScriptAlertBoxButton();
-
         assertTrue(javaScriptAlertsPage.isAlertVisible());
-
         javaScriptAlertsPage.closeAlertBox();
     }
 
     @Test
     public void closeConfirmBoxWithOk() {
         LandingPage landingPage = new LandingPage();
-        landingPage.clickOnStartButton();
-
-        BasicExamplesPage basicExamplesPage = new BasicExamplesPage();
-        basicExamplesPage.clickJavascriptAlertsButton();
-
+        landingPage
+                .clickOnStartButton()
+                .clickJavascriptAlertsButton()
+                .clickJavaScriptConfirmBoxButton();
         JavaScriptAlertsPage javaScriptAlertsPage = new JavaScriptAlertsPage();
-        javaScriptAlertsPage.clickJavaScriptConfirmBoxButton();
-
         assertTrue(javaScriptAlertsPage.isAlertVisible());
-
         javaScriptAlertsPage.closeAlertBox();
-
         assertEquals(javaScriptAlertsPage.getConfirmationMessage(), "You pressed OK!");
     }
 
     @Test
     public void closeConfirmBoxWithCancel() {
         LandingPage landingPage = new LandingPage();
-        landingPage.clickOnStartButton();
-
-        BasicExamplesPage basicExamplesPage = new BasicExamplesPage();
-        basicExamplesPage.clickJavascriptAlertsButton();
-
+        landingPage
+                .clickOnStartButton()
+                .clickJavascriptAlertsButton()
+                .clickJavaScriptConfirmBoxButton();
         JavaScriptAlertsPage javaScriptAlertsPage = new JavaScriptAlertsPage();
-        javaScriptAlertsPage.clickJavaScriptConfirmBoxButton();
-
         assertTrue(javaScriptAlertsPage.isAlertVisible());
-
         javaScriptAlertsPage.dismissAlertBox();
-
         assertEquals(javaScriptAlertsPage.getConfirmationMessage(), "You pressed Cancel!");
     }
 
     @Test
     public void closePromptBoxWithText() {
         LandingPage landingPage = new LandingPage();
-        landingPage.clickOnStartButton();
-
-        BasicExamplesPage basicExamplesPage = new BasicExamplesPage();
-        basicExamplesPage.clickJavascriptAlertsButton();
-
+        landingPage
+                .clickOnStartButton()
+                .clickJavascriptAlertsButton()
+                .clickJavaScriptPromptBoxButton();
         JavaScriptAlertsPage javaScriptAlertsPage = new JavaScriptAlertsPage();
-        javaScriptAlertsPage.clickJavaScriptPromptBoxButton();
-
         assertTrue(javaScriptAlertsPage.isAlertVisible());
-
         javaScriptAlertsPage.sendKeysToPromptBox("test message");
-
         OtherWaits.waitForXSeconds("3");
-
         javaScriptAlertsPage.closeAlertBox();
-
         assertEquals(javaScriptAlertsPage.getPromptBoxMessage(), "You have entered 'test message' !");
     }
-
 }
