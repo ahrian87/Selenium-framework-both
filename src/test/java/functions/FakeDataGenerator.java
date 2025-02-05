de0e1506528ae1e7fe4c8e66442083de778a0130
Maciej Zbydniewski <maciej.zbydniewski@gmail.com>
2025-02-05 08:58:32 +0100

    methods for FakeDataGenerator, methods for AddressesPage

--- src/test/java/functions/FakeDataGenerator.java
+++ src/test/java/functions/FakeDataGenerator.java
@@ -5,12 +5,15 @@ import org.apache.logging.log4j.Logger;
 import com.github.javafaker.Faker;
 
 public class FakeDataGenerator {
-    public static Faker faker;
+    public static Faker faker = new Faker();
 
     private static final Logger logger = LogManager.getRootLogger();
 
     public static String generateFakeEmail() {
-        faker = new Faker();
         return faker.internet().emailAddress();
+    }
+
+    public static String generateFakePhoneNumber() {
+        return faker.phoneNumber().cellPhone();
     }
 }
--- src/test/java/page/objects/AddressesPage.java
+++ src/test/java/page/objects/AddressesPage.java
@@ -1,11 +1,13 @@
 package page.objects;
 
 import driver.manager.DriverManager;
+import functions.FakeDataGenerator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 import waits.WaitForElement;
 
 public class AddressesPage {
@@ -47,5 +49,56 @@ public class AddressesPage {
         WaitForElement.waitUntilElementIsClickable(addNewAddressButton);
         addNewAddressButton.click();
         logger.info("Kliknięto przycisk Add new");
+    }
+
+    public void fillFirstNameField(String firstName) {
+        WaitForElement.waitUntilElementIsClickable(firstNameField);
+        firstNameField.sendKeys(firstName);
+        logger.info("Uzupełniono pole First Name: {}", firstName);
+    }
+
+    public void fillLastNameField(String lastName) {
+        WaitForElement.waitUntilElementIsClickable(lastNameField);
+        lastNameField.sendKeys(lastName);
+        logger.info("Uzupełniono pole Last Name: {}", lastName);
+    }
+
+    public void fillEmailFieldWithRandomValue() {
+        WaitForElement.waitUntilElementIsClickable(emailField);
+        String email = FakeDataGenerator.generateFakeEmail();
+        emailField.sendKeys(email);
+        logger.info("Uzupełniono pole Email: {}", email);
+    }
+
+    public void selectCountry() {
+        WaitForElement.waitUntilElementIsVisible(countrySelect);
+        Select countryDropDown = new Select(countrySelect);
+        countryDropDown.selectByVisibleText("Poland");
+        logger.info("Wybrano kraj: Polska");
+    }
+
+    public void fillCityField(String cityName) {
+        WaitForElement.waitUntilElementIsClickable(cityField);
+        cityField.sendKeys(cityName);
+        logger.info("Uzupełniono pole City Name: {}", cityName);
+    }
+
+    public void fillAddressOneField(String addressOne) {
+        WaitForElement.waitUntilElementIsClickable(addressOneField);
+        addressOneField.sendKeys(addressOne);
+        logger.info("Uzupełniono pole Address 1: {}", addressOne);
+    }
+
+    public void fillZipCodeField(String zipCode) {
+        WaitForElement.waitUntilElementIsClickable(zipCodeField);
+        zipCodeField.sendKeys(zipCode);
+        logger.info("Uzupełniono pole Zipcode: {}", zipCode);
+    }
+
+    public void fillPhoneNumberFieldWithRandomValue() {
+        WaitForElement.waitUntilElementIsClickable(phoneNumberField);
+        String phone = FakeDataGenerator.generateFakePhoneNumber();
+        phoneNumberField.sendKeys(phone);
+        logger.info("Uzupełniono pole Phone number: {}", phone);
     }
 }
